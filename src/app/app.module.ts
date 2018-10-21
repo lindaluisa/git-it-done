10d64887df32896a5794c6b6c57d8fe984e98822
lindaluisa <lindaluisarosenthal@gmail.com>
2018-10-21 23:05:39 +0200

    cleaning code, adding FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { FeedbackEditComponent } from './feedback-list/feedback-edit/feedback-ed
     FeedbackEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

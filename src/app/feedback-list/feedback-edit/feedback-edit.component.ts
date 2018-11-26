52341c51c889dae10acbb5edafaba9434bf58dc3
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2018-11-26 16:51:35 +0100

    injecting service into feedback edit cmp

--- src/app/feedback-list/feedback-edit/feedback-edit.component.ts
+++ src/app/feedback-list/feedback-edit/feedback-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { Wellbeing } from '../../shared/wellbeing.model';
+import { FeedbackListService } from '../feedback-list.service';
 
 @Component({
   selector: 'app-feedback-edit',
@@ -12,7 +13,7 @@ export class FeedbackEditComponent implements OnInit {
 @ViewChild('nameInput') nameInputRef: ElementRef;
 @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  constructor() { }
+  constructor(private feedbacklistService: FeedbackListService) { }
   ngOnInit() {
   }
 
@@ -20,6 +21,7 @@ export class FeedbackEditComponent implements OnInit {
     const wellbeingName = this.nameInputRef.nativeElement.value;
     const stateAmount = this.amountInputRef.nativeElement.value;
     const newWellbeing = new Wellbeing(wellbeingName, stateAmount);
+    this.feedbacklistService.addWellbeing(newWellbeing);
   }
 
 

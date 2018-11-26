ddff4faf73af4562b1adc44a34b6a9cf54ccf6e1
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2018-11-26 17:22:46 +0100

    adding wellbeing states to monomeal model

--- src/app/monomeals/monomeal.model.ts
+++ src/app/monomeals/monomeal.model.ts
@@ -1,11 +1,16 @@
+import { Wellbeing } from "../shared/wellbeing.model";
+
+
 export class Monomeal {
     public name: string;
     public description: string;
     public imagePath: string;
+    public wellbeing: Wellbeing[];
 
-    constructor(name: string, desc: string, imgPath: string) {
+    constructor(name: string, desc: string, imgPath: string, wellbeing: Wellbeing[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imgPath;
+        this.wellbeing = wellbeing;
     }
 }
